8be4febef7c5f07bcdcc2a0a620e9b47916fc93b
garciagaston <gastondeveloper@gmail.com>
2017-08-23 18:10:00 -0300

    quotes

--- src/HomeScreen/index.js
+++ src/HomeScreen/index.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import HomeScreen from "./HomeScreen.js";
-import EditScreenOne from "./EditScreenOne.js";
-import EditScreenTwo from "./EditScreenTwo.js";
 import { StackNavigator } from "react-navigation";
 export default (DrawNav = StackNavigator({
-  HomeScreen: { screen: HomeScreen },
-  EditScreenOne: { screen: EditScreenOne },
-  EditScreenTwo: { screen: EditScreenTwo }
+  HomeScreen: { screen: HomeScreen }
 }));
